bfb968eb6c12c47d7b50aa42d380321be56111be
NatalieBelcher <79141936+NatalieBelcher@users.noreply.github.com>
2023-06-06 14:03:36 +0000

    challenge complete

--- src/app.php
+++ src/app.php
@@ -1,3 +1,11 @@
 <?php
 
 include '_includes.php';
+
+$newMember = new Member();
+$newWorkspace = new Workspace();
+$newChat = new Chat();
+$newMessage = new Message();
+
+// echonl(get_declared_classes());
+echonl($newMember);
