c0ba96807c7dc7154695b79aec0d32d35725a414
NatalieBelcher <79141936+NatalieBelcher@users.noreply.github.com>
2023-06-06 14:25:33 +0000

    challenge complete

--- src/app.php
+++ src/app.php
@@ -3,5 +3,12 @@
 include '_includes.php';
 
 $admin = new Member();
+$admin->username = 'admin-test';
 
 $member = new Member();
+$member->username = 'member-test';
+
+$workspace = $admin->createWorkspace('workspacetest');
+$admin->addWorkspaceMember($member, $workspace);
+$chat = $member->createChat('new chat', $workspace);
+$member->postMessageToChat('Hello there!', $chat);
